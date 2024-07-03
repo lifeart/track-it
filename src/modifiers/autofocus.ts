082b10a8efaebb0d5fea6dd8b5cf09890049b9ad
Alex Kanunnikov <lifeart92@gmail.com>
2024-07-03 21:43:09 +0300

    +

--- src/modifiers/autofocus.ts
+++ src/modifiers/autofocus.ts
@@ -1,8 +1,10 @@
 export function autofocus(node: HTMLInputElement) {
     const frame = requestAnimationFrame(() => {
-        node.focus();
-        node.selectionStart = node.value.length;
-        node.selectionEnd = node.value.length;
+        setTimeout(() => {
+            node.focus();
+            node.selectionStart = node.value.length;
+            node.selectionEnd = node.value.length;
+        });
     });
     return () => {
         cancelAnimationFrame(frame);
